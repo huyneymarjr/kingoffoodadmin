ead7a683e0b117143a0e383e5d8a89856a0cce43
huyneymarjr <quanghuy18082003@gmail.com>
2025-02-06 15:40:43 +0700

    update

--- backend/README.md
+++ backend/README.md
@@ -6,18 +6,3 @@ https://nodejs.org/download/release/v16.20.0/
 
 #### 1. Cài đặt thư viện với câu lệnh: npm i --legacy-peer-dep
 #### 2. Chạy dự án với câu lệnh: npm run dev
-
-### areas-khu vực
-### categories-mô tả danh mục thực đơn
-### customers-khách hàng
-### inventories-Kho 
-### menus-danh mục thực đơn
-### orderdetails-mô tả đơn hàng
-### orders-đơn hàng
-### payments-thanh toán
-### permissions-quyền hạn
-### promotions-khuyến mãi
-### roles-vai trò 
-### tables-bàn ăn
-### transactions-giao dịch
-### users-người dùng
--- backend/src/databases/databases.service.ts
+++ backend/src/databases/databases.service.ts
@@ -74,8 +74,8 @@ export class DatabasesService implements OnModuleInit {
                         role: adminRole?._id
                     },
                     {
-                        name: "I'm Hỏi Dân IT",
-                        email: "hoidanit@gmail.com",
+                        name: "Quang Huy",
+                        email: "quanghuy@gmail.com",
                         password: this.userService.getHashPassword(this.configService.get<string>("INIT_PASSWORD")),
                         age: 96,
                         gender: "MALE",
